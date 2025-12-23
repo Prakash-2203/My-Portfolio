659cca1a5e3b76166e38c3625271608fcdd1a767
Gnanaprakash R <your-github-email@gmail.com>
2025-12-23 11:44:10 +0530

    fifth commit projectsection.jsx

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,8 +3,8 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
+    title: "FarmBridge",
+    description: "A E-commerce website where farmers to sell their farm-produce.",
     image: "/projects/project1.png",
     tags: ["React", "TailwindCSS", "Supabase"],
     demoUrl: "#",
@@ -12,9 +12,9 @@ const projects = [
   },
   {
     id: 2,
-    title: "Orbit Analytics Dashboard",
+    title: "ParkPoint",
     description:
-      "Interactive analytics dashboard with data visualization and filtering capabilities.",
+      "A solution for Drivers to park their vehicle in the busy area.",
     image: "/projects/project2.png",
     tags: ["TypeScript", "D3.js", "Next.js"],
     demoUrl: "#",
@@ -22,9 +22,9 @@ const projects = [
   },
   {
     id: 3,
-    title: "E-commerce Platform",
+    title: "Library Management",
     description:
-      "Full-featured e-commerce platform with user authentication and payment processing.",
+      "A solution for Academic library using modern technologies.",
     image: "/projects/project3.png",
     tags: ["React", "Node.js", "Stripe"],
     demoUrl: "#",
@@ -100,7 +100,7 @@ export const ProjectsSection = () => {
           <a
             className="cosmic-button w-fit flex items-center mx-auto gap-2"
             target="_blank"
-            href="https://github.com/machadop1407"
+            href="https://github.com/Prakash-2203"
           >
             Check My Github <ArrowRight size={16} />
           </a>
